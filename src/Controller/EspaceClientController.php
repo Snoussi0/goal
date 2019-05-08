503f13c91e000f8836101de5ce60d99d2a39dea6
Snoussi Med <medsnoussi0@gmail.com>
2019-05-08 17:28:30 +0100

    final

--- src/Controller/EspaceClientController.php
+++ src/Controller/EspaceClientController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Terrain;
 use App\Entity\Reservation;
+use App\Entity\Agence;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -95,6 +96,79 @@ class EspaceClientController extends AbstractController
         }
        
     }
+
+
+
+        /**
+     * @Route("/espace_client/mes_reservation", name="espace_client_mes_reservation")
+     */
+    public function mes_reservation(Request $request)
+    {
+            $entityManager = $this->getDoctrine()->getManager();
+            $client = $this->getUser()->getClient();
+        
+            
+            $req=" SELECT r FROM App\Entity\Reservation r where r.client= :client ";
+            $query = $entityManager->createQuery($req)->setParameters(array('client'=>$client));
+                
+
+            $reservations= $query->execute();
+
+            return $this->render('espace_client/mes_reservation.html.twig', [
+                'reservations' => $reservations,
+            ]);
+    }
+
+
+
+     /**
+     * @Route("/espace_client/nos_agences", name="espace_client_nos_agences")
+     */
+    public function nos_agences(Request $request)
+    {
+
+        
+        if ($request->isMethod('post')) {
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $nom=$request->request->get('nom');
+            $req='';
+            if($nom != NULL )
+            {
+                $req=" SELECT p FROM App\Entity\Agence p WHERE  p.nom LIKE :nom ";
+                $query = $entityManager->createQuery($req)->setParameters(array('nom'=> '%'.$nom.'%'));
+            }
+            else
+            {
+                
+                $req=" SELECT p FROM App\Entity\Agence p  ";
+                $query = $entityManager->createQuery($req);
+            }
+
+
+
+            $agence= $query->execute();
+        }
+        else
+        {
+            
+        $repository = $this->getDoctrine()->getRepository(Agence::class);
+        $agence = $repository->findAll();
+        }
+
+
+
+        
+        
+        return $this->render('espace_client/nos_agences.html.twig', [
+            'agences' => $agence,
+        ]);
+    }
+
+
+
+
+
 
 
 
